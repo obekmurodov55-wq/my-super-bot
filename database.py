989cdb9b6423c6bd1ba44cbf3535e329fc417592
obekmurodov55-wq <obekmurodov55@gmail.com>
2026-04-21 12:27:47 +0500

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,17 @@
 import sqlite3
 
 def init_db():
-    conn = sqlite3.connect('users.db')
+    conn = sqlite3.connect('music.db')
     cursor = conn.cursor()
     cursor.execute('''
-        CREATE TABLE IF NOT EXISTS users (
-            user_id INTEGER PRIMARY KEY,
-            username TEXT,
-            first_name TEXT
+        CREATE TABLE IF NOT EXISTS songs (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            title TEXT NOT NULL,
+            file_id TEXT NOT NULL
         )
     ''')
     conn.commit()
     conn.close()
+
+if __name__ == "__main__":
+    init_db()
